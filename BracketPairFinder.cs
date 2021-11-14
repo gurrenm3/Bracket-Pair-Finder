98ca39c1224ae0f01831cd7ef52491faf0cf018c
Thomas <thomaspmar@protonmail.com>
2021-11-13 22:29:01 -0800

    Added Async method and XML Doc Comments

--- BracketPairFinder.cs
+++ BracketPairFinder.cs
@@ -3,32 +3,69 @@
 /// </summary>
 class BracketPairFinder
 {
-    private Dictionary<int, int> bracketPairs = new Dictionary<int, int>();
-    private Stack<int> unsetBracketKeys = new Stack<int>();
-    private string text;
-    private char openingBracket;
-    private char closingBracket;
+    /// <summary>
+    /// The original text that the bracket pairs are from
+    /// </summary>
+    public readonly string originalText;
 
+    /// <summary>
+    /// The definition of what an Opening Bracket should be. Used to find the start of the pair.
+    /// </summary>
+    public readonly char openingBracket;
+
+    /// <summary>
+    /// The definition of what an Closing Bracket should be. Used to find the end of the pair.
+    /// </summary>
+    public readonly char closingBracket;
+
+    /// <summary>
+    /// A cache of all the found bracket pairs
+    /// </summary>
+    private Dictionary<int, int> bracketPairs;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BracketPairFinder"/> class, which is used to find all bracket pairs in a string of text.
+    /// </summary>
+    /// <param name="text">Text to search for bracket pairs</param>
+    /// <param name="openingBracket">Definition of what the Opening Bracket should be for each pair</param>
+    /// <param name="closingBracket">Definition of what the Closing Bracket should be for each pair</param>
     public BracketPairFinder(string text, char openingBracket, char closingBracket)
     {
-        this.text = text;
+        this.originalText = text;
         this.openingBracket = openingBracket;
         this.closingBracket = closingBracket;
-        SetAllBracketPairs();
     }
 
-    private void SetAllBracketPairs()
+    /// <summary>
+    /// Gets all of the Bracket Pairs in this text. Results are cached to improve performance on future calls to this method.
+    /// </summary>
+    /// <returns>Upon completing this method will return all of the bracket pairs in this text</returns>
+    public async Task<Dictionary<int,int>> GetBracketPairsAsync()
     {
-        for (int i = 0; i < text.Length; i++)
+        return await Task.Factory.StartNew(() => GetBracketPairs());
+    }
+
+    /// <summary>
+    /// Gets all of the Bracket Pairs in this text. Results are cached to improve performance on future calls to this method.
+    /// </summary>
+    /// <returns>Upon completing this method will return all of the bracket pairs in this text</returns>
+    public Dictionary<int, int> GetBracketPairs()
+    {
+        if (bracketPairs != null)
+            return bracketPairs;
+
+        bracketPairs = new Dictionary<int, int>();
+        var unsetBracketKeys = new Stack<int>(); // used to keep track of all unset bracket pairs, in order of most recent unset pair to oldest.
+        for (int i = 0; i < originalText.Length; i++)
         {
-            if (text[i] == openingBracket)
+            if (originalText[i] == openingBracket)
             {
-                bracketPairs.Add(i, -1);
+                bracketPairs.Add(i, -1); // assigning -1 to the closing bracket index to help identify errors
                 unsetBracketKeys.Push(i);
                 continue;
             }
 
-            if (text[i] == closingBracket)
+            if (originalText[i] == closingBracket)
             {
                 if (!unsetBracketKeys.TryPop(out int key))
                     continue;
@@ -36,18 +73,6 @@ class BracketPairFinder
                 bracketPairs[key] = i;
             }
         }
-    }
-
-    /// <summary>
-    /// Returns a copy of all the bracket pairs.
-    /// </summary>
-    /// <returns></returns>
-    public Dictionary<int, int> GetBracketPairs()
-    {
-        var pairs = new Dictionary<int, int>();
-        for (int i = 0; i < bracketPairs.Count; i++)
-            pairs.Add(bracketPairs.Keys.ElementAt(i), bracketPairs.Values.ElementAt(i));
-
-        return pairs;
+        return bracketPairs;
     }
 }
